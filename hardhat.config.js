da30878ba1977a182b9e144c638eec21a242930a
GrayJiang <Gray_Jy@yeah.net>
2023-03-16 00:36:48 +0800

    feat: finished basic deploying

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,44 @@ require("hardhat-contract-sizer");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL; // https://dashboard.alchemy.com/
+const PRIVATE_KEY = process.env.PRIVATE_KEY; // 注意是账户的私钥 不是公钥 需要在钱包上点击查看私钥
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY; // https://etherscan.io/
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY  // https://coinmarketcap.com/api/
 module.exports = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+      },
+      {
+        version: "0.6.6",
+      },
+    ],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: { chainId: 31337, blockConfirmations: 1 },
+    sepolia: {
+      chainId: 11155111,
+      blockConfirmations: 6,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      1: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: { timeout: 50000 },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
+  },
 };
---
+++ helper-hardhat-config.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+
+const networkConfig = {
+  31337: {
+    name: "localhost",
+    entranceFee: ethers.utils.parseEther("0.01"),
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 本地环境的gas lane无所谓
+    callbackGasLimit: "500000",
+    interval: "30",
+  },
+
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+  11155111: {
+    name: "sepolia",
+    vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625", // 测试网上对应chain link的vrfCoordinatorV2接口的地址  查询地址在上面
+    entranceFee: ethers.utils.parseEther("0.01"),
+    gasLane:
+      "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei Key Hash on page: https://docs.chain.link/vrf/v2/subscription/supported-networks
+    subscriptionId: "0",
+    callbackGasLimit: "500000",
+    interval: "30",
+  },
+};
+
+const developmentChains = ["localhost", "hardhat"];
+
+module.exports = {
+  networkConfig,
+  developmentChains,
+};
